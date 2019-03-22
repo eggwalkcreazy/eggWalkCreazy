3ff4b7c75659094a77e823b82036dc1c839869b0
yangdengxian <1053522419@qq.com>
2019-03-22 09:15:22 +0800

    修改数据

---
+++ js/utils/config.js
@@ -0,0 +1,18 @@
+/**
+ * 配置文件
+ * @author ydx
+ * @date 2019-03-21
+ */
+var Config = {
+	//数据请求地址
+	dataUrl: 'http://c.dituhui.com/maps/1995268/get_lines',
+	//mapbox token
+	accessToken: 'pk.eyJ1IjoiZGVqdWFuIiwiYSI6InAwbkVIVWsifQ.NYFjyoPvZXwj1_hTo8LtEA',
+	API_URL: 'https://api.mapbox.cn',
+	//mapbox style
+	style: 'mapbox://styles/dejuan/cjtf4wjy65iqg1fmv55lcb99a',
+	//地图中心点
+	mapCenter: [121.59369235743857, 31.258559059722138],
+	//出示级别
+	defaultLevel: 15,
+};
